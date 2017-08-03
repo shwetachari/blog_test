8aa091c5be28d62a4aea0e9e950e335692b5ffe8
Shweta Patrachari <shwetachari@gmail.com>
2017-08-03 10:31:18 -0700

    modified responsiveness
    - added dynamic window.onload and window.onresize events
    - adjusted html and css formatting

--- js/main.js
+++ js/main.js
@@ -16,27 +16,48 @@ var dataset = [
 ];
 
 var w = window.innerWidth / 2.5;
-var h = window.innerWidth / 2.5;
+var h = Math.min(w, window.innerHeight);
+var margin, xScale, yScale, svg;
 
-var xScale = d3.scaleLinear()
-                     .domain([0, d3.max(dataset, function(d) { return d[0]; })])
-                     .range([0, w]);
-var yScale = d3.scaleLinear()
-                     .domain([0, d3.max(dataset, function(d) { return d[1]; })])
-                     .range([0, h]);
-var svg = d3.select("body")
-            .append("svg")
-            .attr("width", w)
-            .attr("height", h);
+window.onload = genScatterPlot;
+window.onresize = getDimensions;
 
-svg.selectAll("circle")
-    .data(dataset)
-    .enter()
-    .append("circle")
-    .attr("cx", function(d) {
-      return xScale(d[0]);
-    })
-    .attr("cy", function(d) {
-      return yScale(d[1]);
-    })
-    .attr("r", 3)
+function getDimensions(){
+    w = window.innerWidth / 2.5;
+    h = Math.min(w, window.innerHeight);;
+    xScale.range = [0, w];
+    yScale.range = [0, h];
+    removePlot();
+    genScatterPlot();
+}
+
+function genScatterPlot() {
+  margin = { top: 20, right: 20, bottom: 20, left: 20 };
+
+  xScale = d3.scaleLinear()
+                       .domain([0, d3.max(dataset, function(d) { return d[0]; })])
+                       .range([0, w]);
+  yScale = d3.scaleLinear()
+                       .domain([0, d3.max(dataset, function(d) { return d[1]; })])
+                       .range([0, h]);
+  svg = d3.select("body")
+              .append("svg")
+              .attr("width", w + margin.left + margin.right)
+              .attr("height", h + margin.top + margin.bottom);
+
+  svg.selectAll("circle")
+      .data(dataset)
+      .enter()
+      .append("circle")
+      .attr("cx", function(d) {
+        return xScale(d[0]);
+      })
+      .attr("cy", function(d) {
+        return yScale(d[1]);
+      })
+      .attr("r", 3)
+}
+
+function removePlot() {
+  d3.select("svg").remove();
+}
